d77ed13896087de14301224b4e59d01afe59b798
정재욱 <wjdwodnr5452@naver.com>
2024-07-26 00:26:15 +0900

    검증 직접 처리 - 개발

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -45,11 +45,11 @@ public class ValidationItemControllerV1 {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
 
-        //검증 오류 결과를 보관
+        // 검증 오류 결과를 보관
         Map<String, String> errors = new HashMap<>();
 
-        //검증 로직
-        if (!StringUtils.hasText(item.getItemName())) {
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
             errors.put("itemName", "상품 이름은 필수입니다.");
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
@@ -59,22 +59,22 @@ public class ValidationItemControllerV1 {
             errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
         }
 
-        //특정 필드가 아닌 복합 룰 검증
+        // 특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
-            if (resultPrice < 10000) {
+            if(resultPrice < 10000) {
                 errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
             }
         }
 
-        //검증에 실패하면 다시 입력 폼으로
-        if (!errors.isEmpty()) {
+        // 검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()){
             log.info("errors = {} ", errors);
             model.addAttribute("errors", errors);
             return "validation/v1/addForm";
         }
 
-        //성공 로직
+        // 성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
